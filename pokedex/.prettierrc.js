30cf9b4bfce8b8df8f840b5b8fd12d770fc11092
arianaomi <nlopez@gmail.comm>
2023-07-19 23:02:25 -0600

    pokedex app

--- pokedex/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // arrowParens: 'avoid',
-  // bracketSameLine: true,
-  // bracketSpacing: true,
-  // singleQuote: true,
-  // semi: false,
-  // trailingComma: "all",
-  // jsxSingleQuote: true,
-};
